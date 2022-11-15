ad8b80fdff0413c18483f4a0efd9da887bf748a5
Ushakov Ilya <Riz4rd@gmail.com>
2022-11-15 21:59:43 +0500

    Add Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int x = 1;
+    
+while(x <= number)
+{
+    if (x % 2 == 0)
+    Console.WriteLine($"{x}");
+    x = x + 1;
+        
+}
